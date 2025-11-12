1cc38830adefab4af45d5cef1863f74992b998e3
Andy-Parshyn <andriy.parshyn@doctarigroup.com>
2025-11-12 19:26:11 +0100

    feat: add phone and email management commands to core functionality

--- src/district_9_personal_assistant/core.py
+++ src/district_9_personal_assistant/core.py
@@ -6,6 +6,29 @@ commands_info = (
     "    - name (required): Name of the contact\n"
     "  find_contact\n"
     "    - name (required): Name of the contact to find\n"
+    "  add_phone\n"
+    "    - contact name (required): Name of the contact to add a" +
+    "phone number to\n"
+    "    - phone (required): Phone number to add\n"
+    "  show_phones\n"
+    "    - contact name (required): Name of the contact whose phone" +
+    "numbers to list\n"
+    "  reset_main_phone\n"
+    "    - contact name (required): Name of the contact to reset the" +
+    "main phone number\n"
+    "    - phone (required): Phone number to set as the main phone\n"
+    "  add_email\n"
+    "    - contact name (required): Name of the contact to add an email to\n"
+    "    - email (required): Email address to add\n"
+    "  change_email\n"
+    "    - contact name (required): Name of the contact whose email" +
+    "to change\n"
+    "    - old email (required): Existing email address to replace\n"
+    "    - new email (required): New email address\n"
+    "  remove_email\n"
+    "    - contact name (required): Name of the contact whose " +
+    "email to remove\n"
+    "    - email (required): Email address to remove\n"
     "  add_note\n"
     "    - contact name (required): Name of the contact to add a note to\n"
     "    - title (optional): Brief subject line for the note\n"
@@ -53,6 +76,7 @@ def parse_input(user_input):
     return cmd, *args
 
 
+@input_error
 def run_personal_assistant():
     book = AddressBook()
     print("Welcome to the Personal Assistant!\n")
@@ -61,6 +85,7 @@ def run_personal_assistant():
     while True:
 
         user_input = input("Enter a command: ")
+        # TODO We should use ARGS
         command, *args = parse_input(user_input)
 
         match command:
@@ -68,6 +93,20 @@ def run_personal_assistant():
                 print(book.add_contact())
             case "find_contact":
                 print(book.find_contact())
+            case "add_phone":
+                print(book.add_phone())
+            case "show_phones":
+                print(book.show_phones())
+            case "set_main_phone":
+                print(book.set_main_phone())
+            case "reset_main_phone":
+                print(book.reset_main_phone())
+            case "add_email":
+                print(book.add_email())
+            case "change_email":
+                print(book.change_email())
+            case "remove_email":
+                print(book.remove_email())
             case "add_note":
                 print(book.add_note())
             case "list_notes":
